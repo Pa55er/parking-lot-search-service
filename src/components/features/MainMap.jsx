1201ed0f67ee0109a01bc12ec0e458b8160035f3
programmerJuni <tjrwns2715@naver.com>
2024-10-04 10:31:19 +0900

    main: 맵 잔상 제거

--- src/components/features/MainMap.jsx
+++ src/components/features/MainMap.jsx
@@ -14,12 +14,12 @@ export default function MainMap() {
     const lists = useZustandStore((state) => state.listingTarget);
     const setDetailTarget = useZustandStore((state) => state.setDetailTarget);
     const targetMarker = useZustandStore((state) => state.targetMarker);
-
     const navigate = useNavigate();
 
     useEffect(() => {
         window.kakao.maps.load(() => {
             const mapContainer = document.getElementById("map"); // 지도를 표시할 div
+            mapContainer.innerHTML = "";
 
             const location =
                 targetMarker.index !== -1
@@ -28,6 +28,7 @@ export default function MainMap() {
                           longitude: targetMarker.longitude,
                       }
                     : { latitude: 37.575752, longitude: 126.976823 };
+
             const mapOption = {
                 center: new kakao.maps.LatLng(
                     location.latitude,
@@ -39,14 +40,14 @@ export default function MainMap() {
             const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
             // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
-            var mapTypeControl = new kakao.maps.MapTypeControl();
+            const mapTypeControl = new kakao.maps.MapTypeControl();
 
             // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
             // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
             map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 
             // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
-            var zoomControl = new kakao.maps.ZoomControl();
+            const zoomControl = new kakao.maps.ZoomControl();
             map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
             // 여러개 마커 표시하기
